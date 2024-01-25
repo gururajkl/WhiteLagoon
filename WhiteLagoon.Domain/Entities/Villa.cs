e783e33939acbd4d7e74f49dfe39916254e73d2c
Gururaj KL <gururaj.kl@sasken.com>
2024-01-25 22:38:14 +0530

    Add validation and crud of villa.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,4 +1,6 @@
-﻿namespace WhiteLagoon.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Domain.Entities;
 
 /// <summary>
 /// Model class for the Villa with some properties.
@@ -6,11 +8,16 @@
 public class Villa
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Please provide name")]
     public required string Name { get; set; }
     public string? Description { get; set; }
+    [Display(Name = "Square Feet")]
     public int Sqft { get; set; }
+    [Display(Name = "Price Per Night")]
     public double Price { get; set; }
+    [Range(1, 10, ErrorMessage = "Occupancy should not be more than 10 and less than 1")]
     public int Occupancy { get; set; }
+    [Display(Name = "Image Link")]
     public string? ImageUrl { get; set; }
     public DateTime? CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -16,7 +16,30 @@ public class VillaController : Controller
     public IActionResult Index()
     {
         List<Villa> villas = context.Villas.ToList();
-
         return View(villas);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Villa villa)
+    {
+        // Villa name and description should not be same.
+        if (villa.Name == villa.Description)
+        {
+            ModelState.AddModelError("Name", "Villa name and description should not be same.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            context.Villas.Add(villa);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(villa);
     }
 }
