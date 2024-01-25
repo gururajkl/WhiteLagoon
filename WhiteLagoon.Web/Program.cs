22b107ee923951ffdc15cbdcb7273abd1b456c8f
Gururaj KL <gururaj.kl@sasken.com>
2024-01-25 05:49:45 +0530

    Support EF in the project.

--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add DbContext.
+builder.Services.AddDbContext<ApplicationDbContext>
+(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Build all the services.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
