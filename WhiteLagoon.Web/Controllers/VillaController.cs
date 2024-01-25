2d39a3cad52ac5398cc9fcb439f5a4887fb90818
Gururaj KL <gururaj.kl@sasken.com>
2024-01-25 06:51:55 +0530

    Create villa ui.

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers;
+
+public class VillaController : Controller
+{
+    private readonly ApplicationDbContext context;
+
+    public VillaController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Villa> villas = context.Villas.ToList();
+
+        return View(villas);
+    }
+}
