c24681af1a7324cc41601afe2532fe2de1f35017
Gururaj KL <gururaj.kl@sasken.com>
2024-01-28 20:29:53 +0530

    Implementing Generic repository pattern.

---
+++ WhiteLagoon.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+
+namespace WhiteLagoon.Application.Common.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    /// <summary>
+    /// Gives the <typeparam name="T" /> lists.
+    /// </summary>
+    /// <param name="filter">Lambda expression.</param>
+    /// <param name="includeProperties">The properties need to include. (Props should be case sensitive)</param>
+    /// <returns>Enumerator</returns>
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+
+    /// <summary>
+    /// Gives the <typeparam name="T" />.
+    /// </summary>
+    /// <param name="filter">Lambda expression.</param>
+    /// <param name="includeProperties">The properties need to include.</param>
+    /// <returns>Enumerator</returns>
+    public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+
+    /// <summary>
+    /// Creates new <typeparam name="T" />.
+    /// </summary>
+    /// <param name="T"><typeparam name="T" /> entity.</param>
+    public void Add(T entity);
+
+    /// <summary>
+    /// Deletes the exisiting T.
+    /// </summary>
+    /// <param name="T"><typeparam name="T" /> entity.</param>
+    public void Delete(T entity);
+}
---
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interfaces;
+
+namespace WhiteLagoon.Infrastructure.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+}
