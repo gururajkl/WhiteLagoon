bd79750586e0edff392bc6c829e5056493ece516
akshaykumar.adekar <akshaykumar.adekar@sasken.com>
2024-01-29 17:55:23 +0530

    Update Amenity migrations

---
+++ WhiteLagoon.Infrastructure/Migrations/20240129122206_AddAmenityToDb.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmenityToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VillaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Amenities_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "Id", "Description", "Name", "VillaId" },
+                values: new object[,]
+                {
+                    { 1, null, "Private Pool", 1 },
+                    { 2, null, "Microwave", 1 },
+                    { 3, null, "Private Balcony", 1 },
+                    { 4, null, "1 king bed and 1 sofa bed", 1 },
+                    { 5, null, "Private Plunge Pool", 2 },
+                    { 6, null, "Microwave and Mini Refrigerator", 2 },
+                    { 7, null, "Private Balcony", 2 },
+                    { 8, null, "king bed or 2 double beds", 2 },
+                    { 9, null, "Private Pool", 3 },
+                    { 10, null, "Jacuzzi", 3 },
+                    { 11, null, "Private Balcony", 3 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(775), new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(787) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(789), new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(790) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(791), new DateTime(2024, 1, 29, 17, 52, 6, 47, DateTimeKind.Local).AddTicks(792) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Amenities_VillaId",
+                table: "Amenities",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2678), new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2692) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2694), new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2695) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2696), new DateTime(2024, 1, 27, 10, 42, 37, 427, DateTimeKind.Local).AddTicks(2697) });
+        }
+    }
+}
