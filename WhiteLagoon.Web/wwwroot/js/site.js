d92579f0fe5b0a5a2640e998c8731cac48753804
akshaykumar.adekar <akshaykumar.adekar@sasken.com>
2024-01-30 10:36:00 +0530

    Add toggle theme and modified Home page.

--- WhiteLagoon.Web/wwwroot/js/site.js
+++ WhiteLagoon.Web/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function SwitchTheme() {
+    var htmlTag = document.documentElement;
+    var currentTheme = localStorage.getItem("theme");
 
-// Write your JavaScript code.
+    // Toggle between dark and light themes
+    if (currentTheme === "dark") {
+        htmlTag.removeAttribute("data-bs-theme");
+        localStorage.setItem("theme", "light");
+        toggleIcons("moonIcon", "sunIcon");
+    } else {
+        htmlTag.setAttribute("data-bs-theme", "dark");
+        localStorage.setItem("theme", "dark");
+        toggleIcons("sunIcon", "moonIcon");
+    }
+}
+
+function toggleIcons(showIconId, hideIconId) {
+    document.getElementById(showIconId).style.display = "inline";
+    document.getElementById(hideIconId).style.display = "none";
+}
+
+// Set initial theme and icon on page load
+document.addEventListener("DOMContentLoaded", function () {
+    var currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "dark") {
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+        toggleIcons("sunIcon", "moonIcon");
+    } else {
+        toggleIcons("moonIcon", "sunIcon");
+    }
+});
