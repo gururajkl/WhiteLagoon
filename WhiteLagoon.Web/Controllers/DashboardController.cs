aa2b03de97407dca9673fe64592e8bdf5485b9aa
Gururaj KL <gururaj.kl@sasken.com>
2024-02-10 19:28:17 +0530

    Radial chart feature in admin dashboard.

---
+++ WhiteLagoon.Web/Controllers/DashboardController.cs
@@ -0,0 +1,80 @@
+﻿namespace WhiteLagoon.Web.Controllers;
+
+public class DashboardController : Controller
+{
+    private readonly IUnitOfWork unitOfWork;
+    static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
+    readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+    readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+    public DashboardController(IUnitOfWork unitOfWork)
+    {
+        this.unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult GetTotalBookingRadialChartData()
+    {
+        var totalBookings = unitOfWork.Booking.GetAll(u => u.Status != StaticDetails.StatusPending
+        || u.Status != StaticDetails.StatusCancelled);
+
+        var countByCurrentMonth = totalBookings.Count(b => b.BookingDate >= currentMonthStartDate &&
+        b.BookingDate <= DateTime.Now);
+
+        var countByPreviousMonth = totalBookings.Count(b => b.BookingDate >= previousMonthStartDate &&
+        b.BookingDate <= currentMonthStartDate);
+
+        return Json(GetRadialChartDataModel(totalBookings.Count(), countByCurrentMonth, countByPreviousMonth));
+    }
+
+    public IActionResult GetRegisteredUserChartData()
+    {
+        var totalUser = unitOfWork.ApplicationUser.GetAll();
+
+        var countByCurrentMonth = totalUser.Count(b => b.CreatedAt >= currentMonthStartDate &&
+        b.CreatedAt <= DateTime.Now);
+
+        var countByPreviousMonth = totalUser.Count(b => b.CreatedAt >= previousMonthStartDate &&
+        b.CreatedAt <= currentMonthStartDate);
+
+        return Json(GetRadialChartDataModel(totalUser.Count(), countByCurrentMonth, countByPreviousMonth));
+    }
+
+    public IActionResult GetRevenueChartData()
+    {
+        var totalBookings = unitOfWork.Booking.GetAll(u => u.Status != StaticDetails.StatusPending
+        || u.Status != StaticDetails.StatusCancelled);
+
+        var totalRevenue = Convert.ToInt32(totalBookings.Sum(u => u.TotalCost));
+
+        var countByCurrentMonth = totalBookings.Count(b => b.BookingDate >= currentMonthStartDate &&
+        b.BookingDate <= DateTime.Now);
+
+        var countByPreviousMonth = totalBookings.Count(b => b.BookingDate >= previousMonthStartDate &&
+        b.BookingDate <= currentMonthStartDate);
+
+        return Json(GetRadialChartDataModel(totalRevenue, countByCurrentMonth, countByPreviousMonth));
+    }
+
+    private static RadialBarChartViewModel GetRadialChartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
+    {
+        RadialBarChartViewModel radialBarChartViewModel = new();
+        int increaseDecreaseRatio = 100;
+
+        if (prevMonthCount != 0)
+        {
+            increaseDecreaseRatio = Convert.ToInt32((currentMonthCount - prevMonthCount) / prevMonthCount * 100);
+        }
+
+        radialBarChartViewModel.TotalCount = totalCount;
+        radialBarChartViewModel.CountInCurrentMonth = Convert.ToInt32(currentMonthCount);
+        radialBarChartViewModel.HasRatioIncreased = currentMonthCount > prevMonthCount;
+        radialBarChartViewModel.Series = new int[] { increaseDecreaseRatio };
+
+        return radialBarChartViewModel;
+    }
+}
---
+++ WhiteLagoon.Web/ViewModels/RadialBarChartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WhiteLagoon.Web.ViewModels;
+
+public class RadialBarChartViewModel
+{
+    public decimal TotalCount { get; set; }
+    public decimal CountInCurrentMonth { get; set; }
+    public bool HasRatioIncreased { get; set; }
+    public int[]? Series { get; set; }
+}
